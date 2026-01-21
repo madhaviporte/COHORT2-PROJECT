c0f81245b4dc5718a0217e29962de73aebb2e6b6
madhaviporte <madhaviporte2004@gmail.com>
2026-01-21 23:57:18 +0530

    Change

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import "./app.scss"
 import Dock from "./components/Dock";
 import Nav from "./components/Nav";
+import MacWindow from "./components/windows/MacWindow";
+import Github from "./components/windows/Github";
 
 function App(){
   return(
     <main>
       <Dock/>
       <Nav/>
+      <Github/>
     </main>
   )
 }
