5d33cb5f0a25a1ed7a7d920aa7ba9cea20e7ff4e
madhaviporte <madhaviporte2004@gmail.com>
2026-03-04 17:33:29 +0530

    update

--- Insta-Clone/Backend/src/app.js
+++ Insta-Clone/Backend/src/app.js
@@ -9,7 +9,7 @@ app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(cors({
     credentials: true,
-    origin:"http://localhost:5173"
+    origin:"https://insta-clone-mu-pied.vercel.app"
 }))
 
 // require routes
