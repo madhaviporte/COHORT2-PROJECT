5ff9cc6d024ebacce2b93abda5961ca433629983
madhaviporte <madhaviporte2004@gmail.com>
2026-03-22 21:36:19 +0530

    Animated website

---
+++ Animated website/src/components/home/Video.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div className='h-full w-full'>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="https://res.cloudinary.com/dp4yhgznx/video/upload/v1774171465/69496b2d_z7yfxf.mp4"></video>
+    </div>
+  )
+}
+
+export default Video
---
+++ Animated website/src/pages/Projects.jsx
@@ -0,0 +1,60 @@
+import { useGSAP } from '@gsap/react'
+import ProjectCard from '../components/projects/ProjectCard'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+
+const Projects = () => {
+
+  const projects = [{
+    image1: 'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+  },{
+    image1: 'https://images.unsplash.com/photo-1586281380117-5a60ae2050cc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image2: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  }
+
+]
+
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(function () {
+    gsap.from('.hero', {
+      height: '100px',
+      stagger: {
+        amount: 0.4
+      },
+      scrollTrigger: {
+        trigger: '.lol',
+        start: 'top 100%',
+        end: 'top -150%',
+        scrub: true
+      }
+    })
+  })
+
+  return (
+    <div className='lg:p-4 p-2 mb-[100vh]'>
+      <div className=' pt-[45vh]'>
+        <h2 className='font-[font2] lg:text-[9.5vw] text-7xl uppercase'>Projets</h2>
+      </div>
+      <div className='-lg:mt-20 lol'>
+        {projects.map(function (elem, idx) {
+          return <div key={idx} className='hero w-full lg:h-212.5 mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
+            <ProjectCard image1={elem.image1} image2={elem.image2} />
+          </div>
+        })}
+
+      </div>
+    </div>
+  )
+}
+
+export default Projects
